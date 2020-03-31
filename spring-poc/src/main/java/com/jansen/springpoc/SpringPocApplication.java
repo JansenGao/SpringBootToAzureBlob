39c2741ff99cc81e9d849141be404bdfeb580e44
JansenGao <298066717@qq.com>
2020-03-31 11:58:28 +0800

    Init Spring project.

---
+++ spring-poc/src/main/java/com/jansen/springpoc/SpringPocApplication.java
@@ -0,0 +1,13 @@
+package com.jansen.springpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPocApplication.class, args);
+	}
+
+}
---
+++ spring-poc/src/test/java/com/jansen/springpoc/SpringPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jansen.springpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
